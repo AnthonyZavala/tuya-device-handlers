31769c1a42dbee59841b88ae6a7ba9ce7f7f13ea
epenet <6771947+epenet@users.noreply.github.com>
2026-06-08 10:24:33 +0000

    Add quirk for T&H sensor (jc1afi7ow32okd0h)

---
+++ src/tuya_device_handlers/devices/tdq/tdq_jc1afi7ow32okd0h.py
@@ -0,0 +1,43 @@
+"""Quirk for T & H Sensor with external probe (product_id jc1afi7ow32okd0h).
+
+Tuya does not advertise any datapoints for this device.
+They have been retrieved from the Tuya Developer Portal.
+
+See https://github.com/home-assistant/core/issues/163205.
+"""
+
+from tuya_device_handlers import TUYA_QUIRKS_REGISTRY
+from tuya_device_handlers.builder import DeviceQuirk
+from tuya_device_handlers.const import DPMode
+
+(
+    DeviceQuirk()
+    .applies_to(product_id="jc1afi7ow32okd0h")
+    .add_dpid_integer(
+        dpid=101,
+        dpcode="temp_current",
+        dpmode=DPMode.READ,
+        unit="℃",
+        min=-200,
+        max=600,
+        scale=1,
+        step=1,
+    )
+    .add_dpid_integer(
+        dpid=102,
+        dpcode="humidity_value",
+        dpmode=DPMode.READ,
+        unit="%",
+        min=0,
+        max=100,
+        scale=0,
+        step=1,
+    )
+    .add_dpid_enum(
+        dpid=103,
+        dpcode="battery_state",
+        dpmode=DPMode.READ,
+        enum_range=["low", "middle", "high"],
+    )
+    .register(TUYA_QUIRKS_REGISTRY)
+)
